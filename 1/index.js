867c05e97ce2e84d6236158f303a545b40701421
Joren Broekema <joren.broekema@ing.com>
2020-12-01 12:19:02 +0100

    day 1 both parts

---
+++ 1/index.js
@@ -0,0 +1,40 @@
+import data from "./data.js";
+
+const runPart1 = () => {
+  let output;
+  let computations = 0;
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      computations++;
+      if (data[i] + data[j] === 2020) {
+        output = data[i] * data[j];
+        console.log(
+          `computations: ${computations} for data length ${data.length}`
+        );
+        return output;
+      }
+    }
+  }
+};
+
+const runPart2 = () => {
+  let output;
+  let computations = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      for (let k = j + 1; k < data.length; k++) {
+        computations++;
+        if (data[i] + data[j] + data[k] === 2020) {
+          output = data[i] * data[j] * data[k];
+          console.log(
+            `computations: ${computations} for data length ${data.length}`
+          );
+          return output;
+        }
+      }
+    }
+  }
+};
+console.log(runPart1());
+console.log(runPart2());
